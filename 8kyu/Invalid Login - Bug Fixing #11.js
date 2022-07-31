fd97e920b7293708c08308aec70a14c620f771d4
Falloutdevil <Falloutdevil@ya.ru>
2022-07-31 19:39:47 +0300

    31.07.2022

---
+++ 8kyu/Invalid Login - Bug Fixing #11.js
@@ -0,0 +1,14 @@
+/*
+Your task is simple, search the password string for any injected code (Injected code is any thing that would be used to exploit flaws in the current code, so basically anything that contains || or //) if you find any you must return "Wrong username or password!" because no one likes someone trying to cheat their way in!
+ */
+
+// function validate(username, password){
+//     var database = new Database();
+//     return database.login(username, password);
+// }
+
+
+function validate(username, password) {
+    let valid = new Validator();
+    return valid.login(username, escape(password));
+}
