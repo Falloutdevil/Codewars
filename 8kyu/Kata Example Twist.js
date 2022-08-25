9f98f41df4921d9acecd4422fba55ebf7679c3e9
Falloutdevil <Falloutdevil@ya.ru>
2022-08-25 21:20:06 +0300

    25.08.2022

---
+++ 8kyu/Kata Example Twist.js
@@ -0,0 +1,8 @@
+/*
+DESCRIPTION:
+This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+Add the value "codewars" to the array websites/Websites 1,000 times.
+ */
+
+let websites = new Array(1000).fill('codewars');
