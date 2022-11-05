5b743838e55994d18be39625568bd091320e2f0b
Falloutdevil <Falloutdevil@ya.ru>
2022-11-05 14:28:03 +0300

    05.11.2022

---
+++ 8kyu/Be Concise IV - Index of an element in an array.js
@@ -0,0 +1,18 @@
+/*
+DESCRIPTION:
+Be Concise IV - Index of an element in an array
+Task
+Provided is a function find which accepts two parameters in the following order: array, element and returns the index of the element if found and "Not found" otherwise. Your task is to shorten the code as much as possible in order to meet the strict character count requirements of the Kata. (no more than 85) You may assume that all array elements are unique.
+ */
+
+// function find(array, element) {
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] === element) return i;
+//     }
+//     return "Not found";
+// }
+
+
+const find = (a, el) => {
+    return a.indexOf(el) >= 0 ? a.indexOf(el) : 'Not found';
+}
