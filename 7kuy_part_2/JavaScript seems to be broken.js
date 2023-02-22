ad07651fef73533d143ec198d7f536f25e23c48f
Falloutdevil <Falloutdevil@ya.ru>
2023-02-22 17:35:08 +0300

    22.02.2023

---
+++ 7kuy_part_2/JavaScript seems to be broken.js
@@ -0,0 +1,17 @@
+/*
+DESCRIPTION:
+There is some horribly rotten script that is not required in your project anymore, and it is affecting the way your code is supposed to work.
+
+For some reason you don't have an access to that rotten piece of code but you want to get your code working as expected in any possible way.
+
+Go and get it working!
+
+function semicolonSeparationToCommaSeparation(input) {
+  return input.split(";").join(",");
+}
+ */
+
+function semicolonSeparationToCommaSeparation(input) {
+    let [a, b, c] = input.split(';');
+    return `${a},${b},${c}`;
+}
