8189ac633be168dbbddc16d0e6f4956ea6a211e5
Falloutdevil <Falloutdevil@ya.ru>
2023-07-22 18:00:41 +0300

    22.07.2023

---
+++ 7kuy_part_3/All unique.js
@@ -0,0 +1,20 @@
+/*
+DESCRIPTION:
+Write a program to determine if a string contains only unique characters. Return true if it does and false otherwise.
+
+The string may contain any of the 128 ASCII characters. Characters are case-sensitive, e.g. 'a' and 'A' are considered different characters.
+ */
+
+// function hasUniqueChars(str) {
+//     const seen = {};
+//     for (let i = 0; i < str.length; i++) {
+//         const char = str[i];
+//         if (seen[char]) {
+//             return false;
+//         }
+//         seen[char] = true;
+//     }
+//     return true;
+// }
+
+let hasUniqueChars = (str) => new Set(str).size === str.length;
