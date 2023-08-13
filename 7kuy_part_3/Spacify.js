cc9e0f4c8c5134f48bba50c6f70615c50c792af2
Falloutdevil <Falloutdevil@ya.ru>
2023-08-13 15:19:22 +0300

    13.08.2023

---
+++ 7kuy_part_3/Spacify.js
@@ -0,0 +1,18 @@
+/*
+DESCRIPTION:
+Modify the spacify function so that it returns the given string with spaces inserted between each character.
+
+spacify("hello world") // returns "h e l l o   w o r l d"
+ */
+// function spacify(str) {
+//     let result = '';
+//     for (let i = 0; i < str.length; i++) {
+//         result += str[i] + (i !== str.length - 1 ? ' ' : '');
+//     }
+//     return result;
+// }
+
+function spacify(str) {
+    return str.split("").join(" ");
+}
+
