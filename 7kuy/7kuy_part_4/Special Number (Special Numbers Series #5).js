1e1e0d861ca22afbac02a7388aef560f701751f3
Falloutdevil <Falloutdevil@ya.ru>
2023-10-12 20:02:44 +0300

    12.10.2023

---
+++ 7kuy/7kuy_part_4/Special Number (Special Numbers Series #5).js
@@ -0,0 +1,11 @@
+/*
+DESCRIPTION:
+Definition
+A number is a Special Number if it’s digits only consist 0, 1, 2, 3, 4 or 5
+
+Given a number determine if it special number or not .
+ */
+
+function specialNumber(n) {
+    return /[6-9]/.test(n) ? 'NOT!!' : 'Special!!';
+}
