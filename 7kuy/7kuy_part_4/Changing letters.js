0e31816538f2b48c057c26336ad8ac2489efb9fd
Falloutdevil <Falloutdevil@ya.ru>
2023-10-13 18:08:27 +0300

    13.10.2023

---
+++ 7kuy/7kuy_part_4/Changing letters.js
@@ -0,0 +1,16 @@
+/*
+DESCRIPTION:
+When provided with a String, capitalize all vowels
+
+For example:
+
+Input : "Hello World!"
+
+Output : "HEllO WOrld!"
+
+Note: Y is not a vowel in this kata.
+ */
+
+function swap(string) {
+    return string.replace(/[aeiou]/g, v => v.toUpperCase());
+}
