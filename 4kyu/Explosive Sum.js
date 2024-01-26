afd2d68bcc19e3e670cb75e2ce50bf9eeae23ab0
Falloutdevil <Falloutdevil@ya.ru>
2024-01-26 15:08:45 +0300

    26.01.2024

---
+++ 4kyu/Explosive Sum.js
@@ -0,0 +1,25 @@
+/*How many ways can you make the sum of a number?
+From wikipedia: https://en.wikipedia.org/wiki/Partition_(number_theory)
+
+In number theory and combinatorics, a partition of a positive integer n, also called an integer partition, is a way of writing n as a sum of positive integers. Two sums that differ only in the order of their summands are considered the same partition. If order matters, the sum becomes a composition. For example, 4 can be partitioned in five distinct ways:
+
+4
+3 + 1
+2 + 2
+2 + 1 + 1
+1 + 1 + 1 + 1
+ */
+
+let memo = []
+
+function sum(n, m = n) {
+    if (n === 0) return 1;
+    if (n < 0 || m === 0) return 0;
+    if (memo[n] && memo[n][m]) return memo[n][m];
+    let total = sum(n, m - 1) + sum(n - m, m);
+    if (!memo[n]) {
+        memo[n] = [];
+    }
+    memo[n][m] = total;
+    return total;
+}
