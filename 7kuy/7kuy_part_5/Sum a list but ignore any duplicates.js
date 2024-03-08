5b8658e2da3132ce2297b4e9f166beb20b62cc2b
Falloutdevil <Falloutdevil@ya.ru>
2024-03-08 16:14:14 +0300

    08.03.2024

---
+++ 7kuy/7kuy_part_5/Sum a list but ignore any duplicates.js
@@ -0,0 +1,11 @@
+/*
+DESCRIPTION:
+Please write a function that sums a list, but ignores any duplicate items in the list.
+
+For instance, for the list [3, 4, 3, 6] , the function should return 10.
+ */
+
+function sumNoDuplicates(n) {
+    return n.filter(element => n.indexOf(element) === n.lastIndexOf(element))
+        .reduce((result, element) => result + element, 0)
+}
