286af30dffdb266b33ecb25298166d24c0ec16cf
Falloutdevil <Falloutdevil@ya.ru>
2024-03-11 16:29:54 +0300

    11.03.2024

---
+++ 7kuy/7kuy_part_5/Exclamation marks series 5 Remove all.js
@@ -0,0 +1,19 @@
+/*
+DESCRIPTION:
+Task
+Remove all exclamation marks from the end of words. Words are separated by a single space. There are no exclamation marks within a word.
+
+Examples
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi Hi"
+remove("!!!Hi !!hi!!! !hi") === "!!!Hi !!hi !hi"
+ */
+
+function remove(string) {
+    return string.split(' ')
+        .map(el => el.replace(/!+$/gi, ''))
+        .join(' ');
+}
